ed75f5630df550dc7cffdcf082af3c738de4f9a4
neustupov <neustupov@yandex.ru>
2018-01-27 20:17:50 +0300

    string and numeric variables

---
+++ 02/js/string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username="Катерина";
+message="Взгляни на мир";
+
+var elName=document.getElementById('name');
+elName.textContent=username;
+var elNote=document.getElementById('note');
+elNote.textContent=message;
